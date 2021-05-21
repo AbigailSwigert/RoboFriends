dc3b46963bb2cc8fb073a27389ff48e0f0841a26
AbigailSwigert <abigail@swigertdesigns.com>
2021-05-21 09:59:07 -0500

    switched App.js to functional component with hooks

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,44 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../Components/CardList';
 import './App.css';
 import Header from '../Components/Header';
 import ErrorBoundary from '../Components/ErrorBoundary';
 
-export default class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchField: ''
-        }
-    }
+function App() {
+    
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
 
-    componentDidMount() {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
+            .then(users => setRobots(users));
+    },[])
+
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value);
     };
 
-    onSearchChange = (event) => {
-        this.setState({ searchField: event.target.value })
-    };
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
 
-    render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
-        });
-
-        if(this.state.robots.length === 0) {
-            return <h1 className='tc' style={{marginTop: '40vh'}}>There are no robots</h1>
-        } else {
-            return (
-                <div className='tc'>
-                    <Header onSearchChange = {this.onSearchChange} />
-                    <ErrorBoundary>
-                        <CardList robots = {filteredRobots} />
-                    </ErrorBoundary>
-                </div>
-            );
-        }
-   }
+    if(robots.length === 0) {
+        return <h1 className='tc' style={{marginTop: '40vh'}}>There are no robots</h1>
+    } else {
+        return (
+            <div className='tc'>
+                <Header onSearchChange = {onSearchChange} />
+                <ErrorBoundary>
+                    <CardList robots = {filteredRobots} />
+                </ErrorBoundary>
+            </div>
+        );
+    }
 };
+
+export default App;
